c872277ea584c9e20297b367fd7953c0351d0a45
Alok Prajapati <alokyes55@gmail.com>
2022-11-06 09:10:25 +0530

    Add files for medicARe
    Contains all the files for the project to submit for hackCBS

---
+++ medicAR-main/Assets/Scripts/SceneManagementMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementMenu : MonoBehaviour
+{
+ public void ToHumanBody()
+    {
+        SceneManager.LoadScene("Prognosis");
+    }
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+    public void backToMenu2()
+    {
+        SceneManager.LoadScene("MainMenu2");
+
+    }
+    public void skeleton()
+    {
+        SceneManager.LoadScene("skeleton");
+
+    }
+    public void ToSystem()
+    {
+        SceneManager.LoadScene("System");
+
+    }
+    public void ToOrgans()
+    {
+        SceneManager.LoadScene("Organs");
+
+    }
+    public void ToImmune()
+    {
+        SceneManager.LoadScene("Immune");
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
